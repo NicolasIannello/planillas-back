1be4fe0c76a1d323cc323474db5fc47b9e4ed5b4
NicolasIannello <nicolas.iannello99@gmail.com>
2024-06-02 14:13:25 -0300

    rutas

--- routes/excel.js
+++ routes/excel.js
@@ -1,8 +1,18 @@
 const { Router }=require('express');
-const { getPDF } = require('../controllers/excel');
+const { check }=require('express-validator');
+const { Cargar, Subir } = require('../controllers/excel');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router=Router();
 
-router.get('/pdf',getPDF);
+router.get('/',Cargar);
+
+router.post('/',[
+    check('nombre','el campo es obligatorio').not().isEmpty(),
+    check('date').isDate(),
+    check('fecha','el campo es obligatorio').not().isEmpty(),
+    validarCampos
+],Subir);
+
 
 module.exports=router;
