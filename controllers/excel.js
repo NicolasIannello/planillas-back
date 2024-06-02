711a91be80327d2d46e8ca6a00c011b809af795c
NicolasIannello <nicolas.iannello99@gmail.com>
2024-06-02 14:12:43 -0300

    subir traer / falta pag

--- controllers/excel.js
+++ controllers/excel.js
@@ -1,10 +1,48 @@
 const { response }=require('express');
+const Excel = require('../models/excel');
+const excel = require('../models/excel');
 
-const getPDF= async(req,res = response)=>{
+const Cargar= async(req,res = response)=>{
+    const desde= req.query.desde || 0;
+
+    [ excels, total ]= await Promise.all([
+        Excel.find().skip(desde).limit(25).sort({ date: -1 }),
+        Excel.countDocuments()
+    ]);
 
     res.json({
         ok:true,
+        excels,
+        total
     });
 };
 
-module.exports={getPDF}
+const Subir= async(req,res = response)=>{
+    const {nombre}=req.body;
+
+    try {
+        const existeExcel= await Excel.findOne({nombre});
+        if(existeExcel){
+            return res.json({
+                ok:false,
+                msg:'Ya existe un excel con el mismo nombre'
+            });
+        }
+
+        const excel = new Excel(req.body);
+        await excel.save();
+
+        res.json({
+            ok:true,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg:'error'
+        });
+    }
+};
+
+module.exports={Cargar, Subir}
