99c52e36f8cc4779844390327dc399be8b54be3e
NicolasIannello <nicolas.iannello99@gmail.com>
2024-06-08 20:38:48 -0300

    new name / bodyparser limit

--- planilla.js
+++ planilla.js
@@ -2,16 +2,19 @@ const express =require('express');
 require('dotenv').config();
 const cors=require('cors');
 const {dbConnection} = require('./database/config');
+const bodyParser = require('body-parser');
 
 const app=express();
 
 app.use(cors());
 
-app.use( express.json() );
+//app.use( express.json() );
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 dbConnection();
 
-app.use('/api/excel', require('./routes/excel'));
+app.use('/planilla/excel', require('./routes/excel'));
 
 
 app.listen( process.env.PORT, () =>{
