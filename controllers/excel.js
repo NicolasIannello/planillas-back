dc8f0908723eeb18164d124ac7d4ea2926ed2250
NicolasIannello <nicolas.iannello99@gmail.com>
2024-06-08 20:38:10 -0300

    llenar

--- controllers/excel.js
+++ controllers/excel.js
@@ -1,6 +1,7 @@
 const { response }=require('express');
 const Excel = require('../models/excel');
 const Dato = require('../models/dato');
+const Hoja = require('../models/hoja');
 
 const Cargar= async(req,res = response)=>{
     const desde= req.query.desde || 0;
@@ -32,9 +33,9 @@ const Subir= async(req,res = response)=>{
         const excel = new Excel(req.body);
         await excel.save();
 
-        for (let i = 1; i < req.body.excel.length-1; i++) {
-            const dato = new Dato({id: excel.id , celda: req.body.excel[i].celda, dato: req.body.excel[i].dato});
-            await dato.save();
+        for (let i = 0; i < req.body.nombres.length; i++) {
+            const hoja = new Hoja({id: excel._id , hoja: req.body.nombres[i]});
+            await hoja.save();
         }
 
         res.json({
@@ -52,22 +53,22 @@ const Subir= async(req,res = response)=>{
 };
 
 const CargarDatos= async(req,res = response)=>{
-    const {id}=req.body;
+    // const {id}=req.body;
     
-    const dato = await Dato.find({ 'id': { $eq: id } }, { celda: 1, dato : 1 });
-    let datos=[], cols=[]
+    // const dato = await Dato.find({ 'id': { $eq: id } }, { celda: 1, dato : 1 });
+    // let datos=[], cols=[]
 
-    for (let i = 0; i < dato.length; i++) {
-        if(dato[i].celda[0]=='A'){
-            if(cols.length!=0){
-                datos.push(cols)
-                cols=[]
-            }
-        }
-        cols.push({dato: dato[i].dato, id: dato[i]._id})
+    // for (let i = 0; i < dato.length; i++) {
+    //     if(dato[i].celda[0]=='A'){
+    //         if(cols.length!=0){
+    //             datos.push(cols)
+    //             cols=[]
+    //         }
+    //     }
+    //     cols.push({dato: dato[i].dato, id: dato[i]._id})
 
-        if(i==dato.length-1) datos.push(cols)
-    }
+    //     if(i==dato.length-1) datos.push(cols)
+    // }
 
     res.json({
         ok:true,
@@ -75,4 +76,36 @@ const CargarDatos= async(req,res = response)=>{
     });
 };
 
-module.exports={Cargar, Subir, CargarDatos}
+const Llenar= async(req,res = response)=>{
+    const {id}=req.body;
+
+    try {
+        const existeHoja= await Hoja.find({$and: [{ 'id': { $eq: id } }, { 'hoja': { $eq: req.body.hoja.name } }] });
+        if(!existeHoja){
+            return res.json({
+                ok:false,
+                msg:'Ocurrio un error en la carga de datos'
+            });
+        }
+
+        for (let i = 0; i < req.body.hoja.datos.length; i++) {
+            if(req.body.hoja.datos[i].dato!=undefined){
+                const dato = new Dato({id: id , celda: req.body.hoja.datos[i].celda, dato: req.body.hoja.datos[i].dato});
+                await dato.save();
+            }
+        }
+
+        res.json({
+            ok:true,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg:'error'
+        });
+    }
+};
+
+module.exports={Cargar, Subir, CargarDatos, Llenar}
